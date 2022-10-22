b6b8dffabaad2c3d43478427091904bef4be245d
FakhruddinNalawala <59691527+FakhruddinNalawala@users.noreply.github.com>
2022-10-22 20:57:56 +1100

    Added functionality to fetch current price from steam, and set default threshold as 90% of value (#49)

--- android-app/app/src/main/java/com/steamwhistle/AddToWatchlistActivity.kt
+++ android-app/app/src/main/java/com/steamwhistle/AddToWatchlistActivity.kt
@@ -1,7 +1,10 @@
 package com.steamwhistle
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -12,6 +15,11 @@ import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+import java.net.URL
+
 
 class AddToWatchlistActivity : AppCompatActivity() {
     companion object {
@@ -41,15 +49,50 @@ class AddToWatchlistActivity : AppCompatActivity() {
             true
         }
 
+        var client = OkHttpClient()
+
         adapter = GameAdapter()
         adapter.submitList(searchResults)
         adapter.onGameClickListener = { game ->
             // TODO: Get the threshold from the user.
-            val threshold = 2000
+            var finalPrice = 0
+            try {
+                val SDK_INT = Build.VERSION.SDK_INT
+                if (SDK_INT > 8) {
+                    val policy = ThreadPolicy.Builder()
+                        .permitAll().build()
+                    StrictMode.setThreadPolicy(policy)
+                    val url =
+                        URL("https://store.steampowered.com/api/appdetails/?appids=" + game.appId + "&filters=basic,price_overview&cc=au")
+                    val request = Request.Builder().url(url).build()
+                    val response = client.newCall(request).execute()
+                    val result = response.body?.string().orEmpty()
+                    Log.i("SteamAPIRequestSuccess", result)
+//                Log.i("SteamAPIRequestSuccess", url.toString())
+                    if (!result.isBlank()) {
+                        val priceInfo = JSONObject(result)
+                            .getJSONObject(game.appId.toString())
+                            .getJSONObject("data")
+                            .optJSONObject("price_overview")
+                        finalPrice = if (priceInfo != null) priceInfo.optInt("final") else 0
+                        Log.i(
+                            "SteamAPIRequestSuccess",
+                            "The current price is " + finalPrice.toString()
+                        )
+                    }
+                }
+            } catch (err: Error) {
+                Log.i(
+                    "SteamAPIRequestError",
+                    "Error when executing get request: " + err.localizedMessage
+                )
+            }
+
+            val threshold = (finalPrice * 0.9).toInt()
             val watchlistGame = WatchlistGame(
                 game.appId,
                 game.name,
-                1000,
+                finalPrice,
                 threshold
             )
 
