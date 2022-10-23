16ae5ea94b2c7cbba246a1bd593df3a3ec89cfe2
James Wright <hello@jameswright.me>
2022-10-24 10:19:50 +1100

    Re-comment functions credential
    This isn't, like, a leak of a secret or anything, just recommenting the
    line that includes the already very ignored functions credential file.
    
    I had left it uncommented for when I was testing using the emulator,
    because then it is needed to use FCM, but it doesn't work on the
    deployed functions, so I have re-commented it and re-deployed.
    
    It would probably be better to have a solution that automatically
    detected if it was on the emulator... but now probably isn't the time
    for that.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -13,11 +13,11 @@ import {attemptToCreateGame} from "./utilities/attemptToCreateGame.js";
 // Refer to https://firebase.google.com/docs/admin/setup
 // on how to obtain the Service Account private key
 // If running locally and you want notifications:
-admin.initializeApp({
-  credential: admin.credential.cert("../secret.json"),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert("../secret.json"),
+// });
 
-// admin.initializeApp();
+admin.initializeApp();
 
 const db = admin.firestore();
 
